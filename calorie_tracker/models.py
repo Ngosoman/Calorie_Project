eea3145cae6a72881594d623798229e852f5cc98
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-03 19:42:00 +0300

    Updated Food Models

--- calorie_tracker/models.py
+++ calorie_tracker/models.py
@@ -2,6 +2,7 @@
 # Create your models here.
 from django.db import models
 from django.utils import timezone
+
 class FoodItem(models.Model):
     name = models.CharField(max_length=100)
     calories = models.PositiveIntegerField()
