cd9f4db500659f8b56255e2072bba8b7024d6d22
Tom Mwambingu <[tomchom30@gmail.com]>
2025-08-05 10:11:29 +0300

    Added Views.py

--- calorie_tracker/views.py
+++ calorie_tracker/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import FoodItem
+from django.utils import timezone
+
+def index(request):
+    today = timezone.now().date()
+    foods = FoodItem.objects.filter(date_added=today)
+    total = sum(item.calories for item in foods)
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        calories = request.POST.get("calories")
+        if name and calories:
+            FoodItem.objects.create(name=name, calories=int(calories))
+            return redirect('index')
+
+    return render(request, "calorie_tracker/index.html", {"foods": foods, "total": total})
+
+def delete_item(request, item_id):
+    FoodItem.objects.get(id=item_id).delete()
+    return redirect('index')
+
+def reset_day(request):
+    FoodItem.objects.filter(date_added=timezone.now().date()).delete()
+    return redirect('index')
